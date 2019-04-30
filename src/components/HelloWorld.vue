e0f20ce47f502664cd92342481a50e2623744d2d
lermanori <lermanori@gmail.com>
2019-04-30 16:02:37 +0300

    line breaks -education

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -59,7 +59,8 @@ export default {
   data() {
     return {
       education_text:
-`The Academic College of Tel Aviv-Yafo 10/2016 – present.
+`The Academic College of Tel Aviv-Yafo 
+10/2016 – present.
 Major: Computer Science B.Sc.`,
       //Programming Coursework: Algorithms, Data Structures, Design Patterns, Data-Bases, Object-oriented-programing. ",
       expiriences: [
